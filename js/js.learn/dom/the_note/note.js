f86d2ea1c43858fbc799855cc126b26dd64cfc8e
Anton Golovko <golovko639@gmail.com>
2019-04-26 20:10:30 +0300

    feat: change note work
    when you hover over child element and it doesn't have attribute, note
    not disappear

--- js/js.learn/dom/the_note/note.js
+++ js/js.learn/dom/the_note/note.js
@@ -1,25 +1,34 @@
 document.addEventListener("mouseover", e => {
-	if (!e.target.getAttribute("data-tooltip")) return;
+	let target = e.target;
+	while (!target.getAttribute("data-tooltip") && target !== document.documentElement) {
+		target = target.parentElement;
+	}
+	if (!target.getAttribute("data-tooltip")) return;
 
-	const coords = e.target.getBoundingClientRect();
+	const coords = target.getBoundingClientRect();
 
 	const tooltip = document.createElement("div");
-	tooltip.innerHTML = e.target.getAttribute("data-tooltip");
+	tooltip.innerHTML = target.getAttribute("data-tooltip");
 	tooltip.classList.add("tooltip-12313");
-	tooltip.style.cssText = "position: fixed; padding: 10px; border: 1px solid grey; background: #ececec; border-radius: 10px;" +
-		" left: " + coords.left + "px;";
-	e.target.appendChild(tooltip);
+	tooltip.style.cssText = "position: fixed; padding: 10px; border: 1px solid grey; background: #ececec; border-radius: 10px;";
+	target.appendChild(tooltip);
 
 	if (coords.top - tooltip.offsetHeight > 0) {
 		tooltip.style.top = coords.top - tooltip.offsetHeight + "px";
 	} else {
 		tooltip.style.top = coords.bottom + "px";
 	}
+
+	tooltip.style.left = coords.left + (target.offsetWidth / 2 ) - tooltip.offsetWidth / 2+ "px";
 });
 
 document.addEventListener("mouseout", e => {
-	if (!e.target.getAttribute("data-tooltip")) return;
+	let target = e.target;
+	while (!target.getAttribute("data-tooltip") && target !== document.documentElement) {
+		target = target.parentElement;
+	}
+	if (!target.getAttribute("data-tooltip")) return;
 
-	const tooltip = e.target.querySelector(".tooltip-12313");
-	e.target.removeChild(tooltip);
+	const tooltip = target.querySelector(".tooltip-12313");
+	target.removeChild(tooltip);
 });
